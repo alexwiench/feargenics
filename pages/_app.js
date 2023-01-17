aee1fbe4b9d7d6a777851f3afec311af50513b0f
Alex Wiench <alexwiench@users.noreply.github.com>
2023-01-17 08:46:48 -0600

    Add font with next @next/font feature

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import { Rubik } from '@next/font/google';
+
+// If loading a variable font, you don't need to specify the font weight
+const rubik = Rubik({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
