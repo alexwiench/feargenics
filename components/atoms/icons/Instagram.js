4e90c55c29a3eabdc0c5a8f07bb1f0c30b6001d6
Alex Wiench <alexwiench@users.noreply.github.com>
2023-01-17 08:47:40 -0600

    Add preliminary hero style

--- components/atoms/icons/Instagram.js
+++ components/atoms/icons/Instagram.js
@@ -6,8 +6,8 @@ export default function Instagram() {
 			height="24"
 			viewBox="0 0 24 24"
 			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
+			stroke="rgb(245, 245, 245)"
+			stroke-width="1.5"
 			stroke-linecap="round"
 			stroke-linejoin="round"
 		>
--- components/organisms/HomeHero.js
+++ components/organisms/HomeHero.js
@@ -1,21 +1,49 @@
 import React from 'react';
+import Image from 'next/image';
 import PropTypes from 'prop-types';
 import EpisodeThumbnail from '../atoms/EpisodeThumbnail';
+import styles from './HomeHero.module.css';
+import backgroundImage from '../../public/images/HeaderBackground.jpg';
 
 export default function HomeHero({ data }) {
 	const { film, subtitle, shortDescription, thumbnail, title } = data;
 	const { title: filmTitle, releaseYear } = film;
 	return (
-		<>
-			<h1>
-				{filmTitle} {releaseYear}
-			</h1>
-			<p>{title}</p>
-			<p>{subtitle}</p>
-			<p>{shortDescription}</p>
-			<EpisodeThumbnail thumbnail={thumbnail} filmTitle={title} />
-			<button>Listen to Episode</button>
-		</>
+		<div>
+			{/* 
+				This is a Next.js hacky way of getting image optimization on a background image. 
+				See https://github.com/vercel/next.js/discussions/18357 for details. 
+			*/}
+			<div className={styles.bgWrap}>
+				<Image
+					alt="Header background"
+					src={backgroundImage}
+					placeholder="blur"
+					quality={100}
+					fill
+					sizes="100vw"
+					style={{
+						objectFit: 'cover',
+					}}
+				/>
+			</div>
+			<div className={styles.centerContent}>
+				<div className={styles.contentContainer}>
+					<div className={styles.headerText}>
+						<h1>
+							{filmTitle} {releaseYear}
+						</h1>
+						<p>{title}</p>
+						<p>{subtitle}</p>
+						<p>{shortDescription}</p>
+					</div>
+					<div>
+						<EpisodeThumbnail thumbnail={thumbnail} filmTitle={title} width={550} />
+					</div>
+				</div>
+				{/* <button>Listen to Episode</button> */}
+			</div>
+		</div>
 	);
 }
 
