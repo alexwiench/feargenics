2aa8435b8f6c337b9d343621a070ae036f513a4f
Alex Wiench <alexwiench@users.noreply.github.com>
2023-01-17 20:07:06 -0600

    add priority rendering prop

--- components/atoms/EpisodeThumbnail.js
+++ components/atoms/EpisodeThumbnail.js
@@ -4,18 +4,28 @@ import PropTypes from 'prop-types';
 
 import defaultThumbnail from '../../public/images/default-thumbnail.jpg';
 
-export default function EpisodeThumbnail({ thumbnail, width, filmTitle }) {
-	return <Image src={thumbnail} width={width} height={width} alt={`${filmTitle} thumbnail`} />;
+export default function EpisodeThumbnail({ thumbnail, width, filmTitle, aboveTheFold }) {
+	return (
+		<Image
+			src={thumbnail}
+			width={width}
+			height={width}
+			alt={`${filmTitle} thumbnail`}
+			priority={aboveTheFold}
+		/>
+	);
 }
 
 EpisodeThumbnail.propTypes = {
 	thumbnail: PropTypes.string.isRequired,
 	width: PropTypes.number,
 	filmTitle: PropTypes.string.isRequired,
+	aboveTheFold: PropTypes.bool,
 };
 
 EpisodeThumbnail.defaultProps = {
 	thumbnail: defaultThumbnail,
 	width: 500,
 	filmTitle: 'Movie title missing, fallback',
+	aboveTheFold: false,
 };
